7192bd5b7b17d92305a3fcc67386750a60dc595b
H04K <th.hesters@gmail.com>
2022-09-20 10:20:27 +0200

    modif norm accent

--- NLP_utils/normalize_text.py
+++ NLP_utils/normalize_text.py
@@ -3,6 +3,6 @@ def normalize_text(text:str):
     text = text.str.replace(r"\#","") # replaces hashtags
     text = text.str.replace(r"http\S+","URL")  # remove URL addresses
     text = text.str.replace(r"@","") # Remove @
-    text = text.str.replace(r"[^A-Za-z0-9()!?\'\`\"]", " ")
+    #text = text.str.replace(r"[^A-Za-z0-9()!?\'\`\"]", " ") uncomment to remove also accent and numbers
     text = text.str.replace("\s{2,}", " ")
     return text
